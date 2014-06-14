7ba7f07c327c1a37b3f2fcb5a3d3b736d36239b2
shunsuke wada <letusfly95@gmail.com>
2014-06-14 21:38:56 +0900

    modify

--- spec/sosick-utils_spec.rb
+++ spec/sosick-utils_spec.rb
@@ -4,3 +4,9 @@ require 'sosick/utils'
 describe Sosick::Utils do
   it { should be_true }
 end
+
+describe Sosick::Utils::Validator do
+  it "should return true" do
+    Sosick::Utils::Validator::validate_arguments("production") eq(true)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require 'rubygems'
+require 'bundler/setup'
+require 'sosick/utils'
+
+RSpec::configure do |config|
+  config.mock_framework = :rspec
+end
 
